ae04a657d49fe2979c2270115ac773ceb3371d26
Harlee <hariharanpugazh@gmail.com>
2025-02-26 15:13:45 +0530

    Oh Shanthi

--- render-build.sh
+++ render-build.sh
@@ -5,7 +5,7 @@ echo "🏗️ Installing Python Dependencies..."
 pip install --no-cache-dir -r requirements.txt
 
 echo "🌍 Installing Playwright Dependencies..."
-playwright install --with-deps chromium
+PLAYWRIGHT_BROWSERS_PATH=0 playwright install chromium
 
 echo "🚀 Starting Gunicorn Server..."
 gunicorn backend.wsgi:application --bind 0.0.0.0:$PORT --timeout 120 --workers=1
